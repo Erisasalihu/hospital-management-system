e443b8efc8341d3de53bc783d5a5cfa8e2678030
Elza Bektashi <eb70561@ubt-uni.net>
2026-01-13 01:02:49 +0100

    in progress

---
+++ client/src/pages/Services.jsx
@@ -0,0 +1,12 @@
+export default function ServicesPage() {
+  return (
+    <div className="bg-white text-slate-900">
+      <HeroClinic />
+      <ClinicIntro />
+      <ServicesSection />
+      <CTASection />
+      <FAQ />
+      <ContactBand />
+    </div>
+  );
+}
